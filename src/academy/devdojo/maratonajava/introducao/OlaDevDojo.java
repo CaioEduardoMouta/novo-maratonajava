683c9a6dd51fe8e9c6965ad2899a02d439c68d34
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-01 22:58:11 -0300

    jsf

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,17 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Isso é um comentário <b>Javadoc</b>
+ */
+
 public class OlaDevDojo {
+// Comentário de uma linha
+    /*
+    Comentário
+    de
+    várias
+    linhas
+     */
 
     public static void main(String[] args) {
         System.out.println("HADUKEN");
---
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int , double ,float , char , byte , short, long, boolean
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 4000;
+        float salarioFloat = (float) 4800.00d;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+        System.out.println("A idade é " + idade +" anos");
+        System.out.println(verdadeiro);
+        System.out.println("char "+caractere);
+        System.out.println(salarioFloat);
+
+
+    }
+}
