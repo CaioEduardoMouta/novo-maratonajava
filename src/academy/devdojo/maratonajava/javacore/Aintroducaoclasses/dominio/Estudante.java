d722357de51bb426ad0c80ea60c35b52644ea061
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-07 23:05:37 -0300

    Java

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Estudante {
+    public String nome = "Leonardo Leitão";
+    public int idade = 30;
+    public char sexo;
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "Fiat";
+        carro1.modelo = "Argo";
+        carro1.ano = 2020;
+
+        carro2.nome = "Fiat";
+        carro2.modelo = "Punto";
+        carro2.ano = 2020;
+
+        //carro1 = carro2;
+
+        System.out.println("Carro 1");
+        System.out.println(carro1.nome);
+        System.out.println(carro1.modelo);
+        System.out.println(carro1.ano);
+
+        System.out.println("\nCarro 2");
+        System.out.println(carro2.nome);
+        System.out.println(carro2.modelo);
+        System.out.println(carro2.ano);
+
+    }
+}
