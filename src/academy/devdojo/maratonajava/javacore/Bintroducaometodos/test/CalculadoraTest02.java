4249e2c09336b4aad2a6e258ab5e14541352394d
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-08 22:42:47 -0300

    Java

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int num = (int)100;
+        calculadora.multiplicaDoisNumeros(4,3.2f);
+    }
+}
