e5473af940e89ae24a718114688aafbfaa552ec2
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-12 13:09:24 -0300

    java

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/Test/EscolaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/Test/EscolaTest01.java
@@ -4,6 +4,6 @@ import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Professor;
 
 public class EscolaTest01 {
     public static void main(String[] args) {
-        Professor professor = new Professor("Caio");
+        Professor professor = new Professor("Caio")s;
     }
 }
