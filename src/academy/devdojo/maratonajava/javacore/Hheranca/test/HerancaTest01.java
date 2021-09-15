e7b1f8ebc40859bfb589a3b3757ce33d233b781c
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-14 23:01:20 -0300

    Java

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.Dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.Dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.Dominio.Pessoa;
+
+public class HerancaTest01 {
+
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua ");
+        endereco.setCep("08030-590");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Caio");
+        pessoa.setCpf("11234-221");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Eduardo");
+        funcionario.setCpf("11111");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(22222);
+        System.out.println("-------------");
+        funcionario.imprime();
+    }
+}
