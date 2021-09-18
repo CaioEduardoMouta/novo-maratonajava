66d6aed8b02f41f0b9173129a057356d7efc939a
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-17 21:44:46 -0300

    java

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -1,12 +1,12 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
-
+//Quando colocado como final não pode ser extendida
 public class Carro {
     private String nome;
     public static final double VELOCIDADE_LIMITE = 250;
     public final Comprador COMPRADOR = new Comprador();
 
-    public Comprador getCOMPRADOR() {
-        return COMPRADOR;
+    public final void imprime(){
+        System.out.println(this.nome);
     }
 
     public String getNome() {
