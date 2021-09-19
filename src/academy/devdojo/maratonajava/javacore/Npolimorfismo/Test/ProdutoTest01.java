8a8e82fd353638b85f126d92ca6c11f298491dd1
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-19 09:30:28 -0300

    JAVA

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/Test/ProdutoTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.Test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest01 {
+
+    public static void main(String[] args) {
+        Computador computador = new Computador("NUC10i7", 11000);
+        Tomate tomate = new Tomate("Tomate do DevDojo",10);
+
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+        System.out.println("--------------");
+        CalculadoraImposto.calcularImpostoComputador(computador);
+
+
+
+    }
+}
