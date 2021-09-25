b5c9ba3b244e3999c21aba98de66f8f923ab0fdc
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-24 22:15:25 -0300

    Java

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/dominio/Funcionario.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.dominio;
+
+import java.io.FileNotFoundException;
+
+public class Funcionario {
+
+    public void salvar() throws LoginInvalidoException, ArithmeticException {
+        System.out.println("salvando funcionario");
+    }
+}
