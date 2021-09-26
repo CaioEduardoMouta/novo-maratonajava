7d91d7a30e1593841c2f1a3d86dd64aadf9e35ea
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-09-25 23:23:38 -0300

    java

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+
+    public static void main(String[] args) {
+        String nome = "Caio";
+        String nome2 = "Caio";
+        System.out.println(nome == nome2);
+    }
+}
