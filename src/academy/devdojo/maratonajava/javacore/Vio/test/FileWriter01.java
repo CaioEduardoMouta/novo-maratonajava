e38b0859990ce470e1ce435527a8720c22d4de39
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-10-04 22:56:04 -0300

    java

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriter01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriter01 {
+
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true)){
+            fw.write("Caio Mouta");
+
+            fw.flush();
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+
+    }
+}
