d714ff5a6e155f4eed7b6a3a29090a6921365a9d
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-10-28 23:23:30 -0300

    java

--- src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
@@ -1,48 +1,60 @@
 package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
 
 class ThreadExample extends Thread {
-    private char c;
+    private final char c;
 
-    public ThreadExample(char c){
+    public ThreadExample(char c) {
         this.c = c;
     }
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        //System.out.println(Thread.currentThread().getName());
         for (int i = 0; i < 500; i++) {
             System.out.print(c);
-            if(i % 100 == 0){
+            if (i % 100 == 0) {
                 System.out.println();
             }
         }
     }
 }
 
-class ThreadExampleRunnable implements  Runnable {
+class ThreadExampleRunnable implements Runnable{
+    private char c;
+
+    public ThreadExampleRunnable(char c) {
+        this.c = c;
+    }
 
     @Override
     public void run() {
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0) {
+                System.out.println();
+            }
+        }
+    }
 
     }
-}
 
 
-public class ThreadTest01 {
-    public static void main(String[] args) {
+    public class ThreadTest01 {
+        public static void main(String[] args) {
 //        ThreadExample t1 = new ThreadExample('A');
 //        ThreadExample t2 = new ThreadExample('B');
 //        ThreadExample t3 = new ThreadExample('C');
 //        ThreadExample t4 = new ThreadExample('D');
-        Thread t1 = new Thread(new ThreadExampleRunnable('A'));
-        Thread t2 = new Thread(new ThreadExampleRunnable('B'));
-        Thread t3 = new Thread(new ThreadExampleRunnable('C'));
-        Thread t4 = new Thread(new ThreadExampleRunnable('D'));
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-
+            Thread t1 = new Thread(new ThreadExampleRunnable('A'),"T1A");
+            Thread t2 = new Thread(new ThreadExampleRunnable('B'),"T2B");
+            Thread t3 = new Thread(new ThreadExampleRunnable('C'),"T3C");
+            Thread t4 = new Thread(new ThreadExampleRunnable('D'),"T4D");
+            t4.setPriority(Thread.MAX_PRIORITY);
+            t1.start();
+            t2.start();
+            t3.start();
+            t4.start();
+            System.out.println("################" + Thread.currentThread().getName());
 
     }
 }
