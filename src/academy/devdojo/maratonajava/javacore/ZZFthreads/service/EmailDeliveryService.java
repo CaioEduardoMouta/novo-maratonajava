a7a932228302635cf0163df20800b977734d69a9
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-10-31 00:56:49 -0300

    java

--- src/academy/devdojo/maratonajava/javacore/ZZFthreads/service/EmailDeliveryService.java
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/service/EmailDeliveryService.java
@@ -18,11 +18,13 @@ public class EmailDeliveryService implements Runnable{
             try{
                 String email = members.retrieveEmail();
                 if(email == null) continue;
-                
+                System.out.println(threadName + "enviando email para "+email);
                 Thread.sleep(2000);
+                System.out.println(threadName + " enviou email com sucesso para "+email);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
         }
+        System.out.println("Todos os emails foram enviados com sucesso");
     }
 }
