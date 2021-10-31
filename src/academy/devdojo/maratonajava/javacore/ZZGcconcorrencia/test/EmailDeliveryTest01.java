8b77801c764e8ead6cc2cad0d302a587da827f84
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-10-31 09:01:52 -0300

    java

--- src/academy/devdojo/maratonajava/javacore/ZZGcconcorrencia/test/EmailDeliveryTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZGcconcorrencia/test/EmailDeliveryTest01.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
+package academy.devdojo.maratonajava.javacore.ZZGcconcorrencia.test;
 
 import academy.devdojo.maratonajava.javacore.ZZFthreads.dominio.Members;
 import academy.devdojo.maratonajava.javacore.ZZFthreads.service.EmailDeliveryService;
@@ -8,8 +8,8 @@ import javax.swing.*;
 public class EmailDeliveryTest01 {
     public static void main(String[] args) {
         Members members = new Members();
-        Thread caio = new Thread(new EmailDeliveryService(members), "Caio");
-        Thread mouta = new Thread(new EmailDeliveryService(members), "Mouta");
+        Thread caio = new Thread(new EmailDeliveryService(members), "Caio ");
+        Thread mouta = new Thread(new EmailDeliveryService(members), "Mouta ");
         caio.start();
         mouta.start();
         while (true){
