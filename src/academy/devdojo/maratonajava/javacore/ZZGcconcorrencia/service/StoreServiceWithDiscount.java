1089b794ef31ec8c3e9b2579addcde0412dea0fe
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-11-05 23:46:51 -0300

    java

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGcconcorrencia/service/StoreServiceWithDiscount.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.ZZGcconcorrencia.service;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class StoreServiceWithDiscount {
+
+    public double getPriceSync(String storeName){
+        return priceGenerator();
+    }
+
+
+    private double priceGenerator(){
+        delay();
+        return ThreadLocalRandom.current().nextInt(1,500) * 10;
+    }
+
+    private void delay(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
