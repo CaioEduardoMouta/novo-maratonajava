dee4b7450113e85480e29357287393a5f36adc96
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-11-14 15:21:16 -0300

    Java

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZKjunit/dominio/Employee.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.ZZKjunit.dominio;
+
+public class Employee {
+    private String id;
+
+    public Employee(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
